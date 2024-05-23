857af0bee8272f7d873e9cc13a6650235c5fc014
Lerato <78935974+Lerato6221@users.noreply.github.com>
2024-05-23 22:09:56 +0200

    Python script is back

---
+++ Practicals/python/mainFunc.py
@@ -0,0 +1,18 @@
+# This is a Demo for Def.
+#The intention of this code is to allow #
+# People to Register at a Mashonisa website, the their loan application wil be processed
+# Based on the following criteria
+#
+# 0 They must register
+# 1 They're over 18
+# 2 They have a stable job.
+# 3 They at least earn R13, 000 after tax
+# 4 The site should calculate affordibility 
+# 4 to qualify the client must be left with 7k
+
+def mashonisa():
+
+
+
+if __name__=='__main__':
+    mashonisa()
